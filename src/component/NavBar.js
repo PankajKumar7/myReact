669b3f50c1a41c5c0100dd45f4d183a16b1cf82a
Pankaj <designerui2020@gmail.com>
2020-07-04 20:47:14 +0530

    add custom font

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
   return (
 
     <Navbar bg="dark" expand="lg" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="/home" className="logoName">Pnakaj kumar</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav" >
     <Nav className="mr-auto" >
